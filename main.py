6c1cfa7b112d7ecb2df5874333a9ce1aa490627d
Vish Singh <vishalicious213@gmail.com>
2020-10-05 21:24:28 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+""""
+Change the assignment statements below so that all of the print function calls print True.
+"""
+# change these values so that all of the print statements print True
+number = 16
+second_number = not 10
+first_list = [1,2,3]
+second_list = [1,2]
+# only change the code above this line
+
+# all of these print statements must print True by only changing the assignment statements above
+print(number > 15)
+print(first_list)
+print(len(second_list) == 2)
+print(len(first_list) + len(second_list) == 5)
+print(first_list and first_list[0] == 1)
+print(not second_number)
